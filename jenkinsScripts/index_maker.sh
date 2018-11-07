2c5e142d03407bd06d11febc9cb05c21755fc705
root <39826573+ltkum@users.noreply.github.com>
2018-11-07 17:06:37 +0000

    the css template seems to remove simple strings from listings. for now, it works but this <a> is ugly as hell. I'm not a frontend dev, so it kinda works for me. People will try to click a link that leads nowhere, though

--- jenkinsScripts/index_maker.sh
+++ jenkinsScripts/index_maker.sh
@@ -80,7 +80,7 @@ virtualenv ${venv_path}
 ${pip_path} install Mako==1.0.7
 #HERE : TODO : write begin of template.
 
-echo "<!DOCTYPE html>" >>${index}
+echo "<!DOCTYPE html>" >${index}
 echo "" >>${index}
 echo "<head>" >>${index}
 echo "  <meta charset=\"utf-8\">" >>${index}
@@ -88,9 +88,9 @@ echo "  <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=\$start\">
 echo "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">" >>${index}
 echo "  <title>Vector Tiles Styles</title>" >>${index}
 echo "" >>${index}
-echo "  <link rel=\"stylesheet\" href=\"files/css/vendors.css\">" >>${index}
-echo "  <link rel=\"stylesheet\" href=\"files/css/admin1.css\">" >>${index}
-echo "  <link rel=\"stylesheet\" href=\"files/css/print.css\">" >>${index}
+echo "  <link rel=\"stylesheet\" href=\"/files/css/vendors.css\">" >>${index}
+echo "  <link rel=\"stylesheet\" href=\"/files/css/admin1.css\">" >>${index}
+echo "  <link rel=\"stylesheet\" href=\"/files/css/print.css\">" >>${index}
 echo "" >>${index}
 echo "</head>" >>${index}
 echo "" >>${index}
@@ -107,7 +107,7 @@ echo "            </nav>" >>${index}
 echo "          </section>" >>${index}
 echo "        </div>" >>${index}
 echo "        <a href=\"#\" class=\"brand hidden-xs\" title=\"back to home\">" >>${index}
-echo "          <img src=\"files/img/logo-CH.png\" alt=\"back to home\" />" >>${index}
+echo "          <img src=\"/files/img/logo-CH.png\" alt=\"back to home\" />" >>${index}
 echo "          <h1>${title}<h1>" >>${index}
 echo "        </a>" >>${index}
 echo "    </header>" >>${index}
@@ -128,7 +128,7 @@ echo "" >>${index}
 echo "<h1>List of available styles</h1>" >>${index}
 echo "  <div id=\"navigation\" style=\"margin-bottom:14px;\"></div>" >>${index}
 echo "  <div id=\"listing\">" >>${index}
-echo "  <ul>" >>${index}
+echo "    <ul>" >>${index}
 
 
 dirname="${resource_dir##*/}"
@@ -142,25 +142,30 @@ for jsonfile in "${resource_dir}"/*/*/${resource_type}.json ; do
     let rel_url_length-=${length_of_filename}
     relative_url=${relative_url:0:${rel_url_length}}
     ${mako_path} --var "servername"="${servername}" --var "protocol"="${protocol}" --var "${resource_type}_json"="${protocol}://${servername}/${dirname}${relative_url}/${resource_type}.json" --var "layername"="${style_name}" ${template_used} > ${resource_dir}${relative_url}/index.html
-    echo "        <li>" >> ${index} 
-    echo "          ${style_name} --> <a href=\"/${dirname}${relative_url}/${resource_type}.json\">\"JSON\"</a>   <a href=\"/${dirname}${relative_url}/index.html\">\"VIEWER\"</a>" >> ${index}
-    echo "        </li>" >> ${index}
+    echo "      <li>" >> ${index} 
+    echo "      <a>${style_name}</a> <a href=\"/${dirname}${relative_url}/${resource_type}.json\">JSON</a>   <a href=\"/${dirname}${relative_url}/index.html\">VIEWER</a>" >> ${index}
+    echo "      </li>" >> ${index}
   fi
 done
-
- 71     </div>
- 72       </div>
- 73         </div>
- 74       </div>
- 75     </div>
- 76         <footer>
- 77           <address>
- 78         <span class="hidden-xs">Swiss Confederation</span>
- 79         <nav class="pull-right">
- 80           <ul>
- 81 
- 82             <li><a href="mailto:info@geo.admin.ch">Contact</a></li>
- 83           </ul>
- 84         </nav>
-      </address>
+echo "    </ul>" >> ${index}
+echo "  </div>" >> ${index}
+echo "    </div>" >> ${index}
+echo "      </div>" >> ${index}
+echo "        </div>" >> ${index}
+echo "      </div>" >> ${index}
+echo "    </div>" >> ${index}
+echo "        <footer>" >> ${index}
+echo "          <address>" >> ${index}
+echo "        <span class=\"hidden-xs\">Swiss Confederation</span>" >> ${index}
+echo "        <nav class=\"pull-right\">" >> ${index}
+echo "          <ul>" >> ${index}
+echo "" >> ${index}
+echo "             <li><a href=\"mailto:info@geo.admin.ch\">Contact</a></li>" >> ${index}
+echo "           </ul>" >> ${index}
+echo "        </nav>" >> ${index}
+echo "      </address>" >> ${index}
+echo "    </footer>" >> ${index}
+echo "  </div>" >> ${index}
+echo "</body>" >> ${index}
+echo "</html>" >> ${index}
 
