206b1c1edc78bbf69b02a9151a6452df0e2103bc
root <39826573+ltkum@users.noreply.github.com>
2019-02-01 15:09:46 +0000

    index maker now makes an index in the correct place with the correct resources

--- jenkinsScripts/index_maker.sh
+++ jenkinsScripts/index_maker.sh
@@ -8,14 +8,14 @@ function usage {
   echo "Usage:"
   echo
   echo "-h  : show you this output here."
-  echo -e "-d \t : the root directory to where your styles directories are stored."
-  echo -e "-t \t : the type of resource you're looking for (style or tileset)"
-  echo -e "-p \t : the protocol used to access the web server on which the files will be deployed (http or https)"
-  echo -e "-s \t: the server address where files will be accessed"
-  echo -e "example usage \t: ./jenkinsScripts/index_maker.sh -p \"/var/local/vectortiles/gl-styles\" -t \"style\""
+  echo -e "-d \t : the directory where you want to put your index in the efs."
+  echo -e "-t \t : the type of resource you're looking for (style or mbtiles)"
+  echo -e "-f \t : the directory where your resources are stored."
+  echo -e "-s \t : the user:server parameters for the scp command"
+  echo -e "example usage \t: jenkinsScripts/index_maker.sh -d \"/var/local/efs-dev/vectortiles/styles\" -t \"style\" -f \"/var/local/efs-dev/vectortiles/styles\" -s \"geodata@geodatasync\""
 }
 
-while getopts :ht:d:s:p: opt "$@";do
+while getopts :ht:d:s:f: opt "$@";do
   case ${opt} in
     h)
       usage
@@ -24,14 +24,14 @@ while getopts :ht:d:s:p: opt "$@";do
     t)
       resource_type=${OPTARG}
       ;;
-    d)
+    f)
       resource_dir=${OPTARG}
       ;;
-    s)
-      servername=${OPTARG}
+    d)
+      output_path=${OPTARG}
       ;;
-    p)
-      protocol=${OPTARG}
+    s)
+      scp_parameters=${OPTARG}
       ;;
     \?)
       echo "invalid option : -${OPTARG}" >&2
@@ -44,41 +44,35 @@ while getopts :ht:d:s:p: opt "$@";do
   esac
 done
 
-#TODO: trap to clean venv, venv and mako creation, mako templating later
-
-
-index=${resource_dir}"/index.html"
+#TODO : check what this does exactly and generate a correct index, with the correct rights in the correct place.
+index="./index.html"
 title=""
 case ${resource_type} in
   style)
   title="List of Gl Styles"
   title_fr=""
   title_de=""
-  template_used="./jenkinsScripts/templates/style_viewer.html.in"
+  resource="style.json"
   ;;
-  tileset)
+  mbtiles)
   title="List of Map Box Tiles Datasets"
   title_fr=""
   title_de=""
-  template_used="./jenkinsScripts/templates/mbtiles_viewer.html.in"
+  resource="tiles.mbtiles"
   ;;
   *)
   (>&2 echo "ERROR: UNSUPPORTED RESOURCE TYPE \"${resource_type}\"")
   exit 2
   ;;
 esac
-venv_path=".venv"
-pip_path="${venv_path}/bin/pip"
-mako_path="${venv_path}/bin/mako-render"
+
+
 function cleanup {
-  rm -rf "${venv_path}" || :
-}
+  rm ${index}
+} 
 
 trap cleanup SIGHUP SIGINT SIGTERM EXIT
 
-virtualenv ${venv_path}
-${pip_path} install Mako==1.0.7
-#HERE : TODO : write begin of template.
 
 echo "<!DOCTYPE html>" >${index}
 echo "" >>${index}
@@ -132,18 +126,25 @@ echo "    <ul>" >>${index}
 
 
 dirname="${resource_dir##*/}"
-for jsonfile in "${resource_dir}"/*/*/${resource_type}.json ; do
+
+
+for jsonfile in "${resource_dir}"/*/*/${resource} ; do
   if [ -f "${jsonfile}" ]; then
-    style_name=$(jq '.name' ${jsonfile})
     let length_of_filename=${#resource_type}
     let length_of_filename+=6   
     relative_url="${jsonfile:${#resource_dir}}"
     let rel_url_length=${#relative_url}
     let rel_url_length-=${length_of_filename}
-    relative_url=${relative_url:0:${rel_url_length}}
-    ${mako_path} --var "servername"="${servername}" --var "protocol"="${protocol}" --var "${resource_type}_json"="${protocol}://${servername}/${dirname}${relative_url}/${resource_type}.json" --var "layername"="${style_name}" ${template_used} > ${resource_dir}${relative_url}/index.html
-    echo "      <li>" >> ${index} 
-    echo "      <a>${style_name}</a> <a href=\"/${dirname}${relative_url}/${resource_type}.json\">JSON</a>   <a href=\"/${dirname}${relative_url}/index.html\">VIEWER</a>" >> ${index}
+    relative_url=${relative_url:0:${rel_url_length}-1}
+    if  [ ${resource} = "style.json" ]; then
+      style_name=$(jq '.name' ${jsonfile})
+      echo "      <li>" >> ${index} 
+      echo "      <a>${style_name}</a> <a href=\"/${dirname}${relative_url}/${resource_type}.json\">JSON</a>   <a href=\"/${dirname}${relative_url}/?vector\">VIEWER</a>" >> ${index}
+    else
+      echo "      <li>" >> ${index} 
+      echo "      <a>${relative_url:1:${#relative_url}-6}</a> <a href=\"/${dirname}${relative_url}.json\">JSON</a>   <a href=\"/${dirname}${relative_url}/\">VIEWER</a>" >> ${index}
+
+    fi
     echo "      </li>" >> ${index}
   fi
 done
@@ -168,4 +169,7 @@ echo "    </footer>" >> ${index}
 echo "  </div>" >> ${index}
 echo "</body>" >> ${index}
 echo "</html>" >> ${index}
+
+scp ${index}  ${scp_parameters}:${output_path}/index.html
+
 
