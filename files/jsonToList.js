b51a21f02036d3b56786e89bd2e0e265224410e4
root <39826573+ltkum@users.noreply.github.com>
2019-03-01 15:05:53 +0000

    working with the new 'template_url' field. Deployed on int.

--- files/jsonToList.js
+++ files/jsonToList.js
@@ -11,7 +11,7 @@ function create_list(resource) {
       var jsonName = resource == "mbtiles" ? "tileJSON" : "style json" ;
       var jsonSuffix = resource == "mbtiles" ? ".json" : "/style.json";
       var viewerSuffix = resource == "mbtiles" ? "#6/46/7" : "";
-
+      var template = data["template_url"];
       if (typeof(data[resource]) == 'object'){
         for (var layer in data[resource]) {
           var liNode = document.createElement("LI");
@@ -19,20 +19,21 @@ function create_list(resource) {
           liNode.appendChild(textNode);
           ulNode.appendChild(liNode);
           var internalUlNode=document.createElement("UL");
-          for (var version in data[resource][layer]['versions']) {
+          for (var versionIdx in data[resource][layer]['versions']) {
+            var version = data[resource][layer]['versions'][versionIdx];
             var internalLiNode = document.createElement("LI");
-            var versionTextNode = document.createTextNode(data[resource][layer]['versions'][version] + '    ');
+            var versionTextNode = document.createTextNode(version + '    ');
             var internalLinkJson = document.createElement("A");
             var jsonText= document.createTextNode(jsonName + '    ');
             internalLinkJson.appendChild(jsonText);
             internalLinkJson.title = jsonName;
-            internalLinkJson.href = '/'+resource+'/'+layer+'/'+data[resource][layer]['versions'][version] + jsonSuffix ;
+            internalLinkJson.href = template.replace("{resource}",resource).replace("{name}",layer).replace("{version}",version).replace("{resource_type}",jsonSuffix) ;
 
             var internalLinkViewer = document.createElement("A");
             var viewerText = document.createTextNode(viewerName);
             internalLinkViewer.appendChild(viewerText);
             internalLinkViewer.title = viewerName;
-            internalLinkViewer.href = '/'+resource+'/'+layer+'/'+data[resource][layer]['versions'][version] +'/viewer.html' + viewerSuffix;
+            internalLinkViewer.href = template.replace("{resource}",resource).replace("{name}",layer).replace("{version}",version).replace("{resource_type}","/viewer.html") + viewerSuffix ;
 
             internalLiNode.appendChild(versionTextNode);
             internalLiNode.appendChild(internalLinkJson);
